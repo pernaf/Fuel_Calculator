b7abae9a3e803accbcc578dfd48ef45150012107
Bowie <marketing@letscode.com.br>
2024-10-07 22:25:34 -0300

    at 2: tela preço combustível e tela consumo do carro

---
+++ app/src/main/java/com/example/fuel_calculator/ConsumoCarro.kt
@@ -0,0 +1,13 @@
+package com.example.fuel_calculator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConsumoCarro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_consumo_carro)
+
+    }
+}
--- app/src/main/java/com/example/fuel_calculator/PriceFuel.kt
+++ app/src/main/java/com/example/fuel_calculator/PriceFuel.kt
@@ -1,12 +1,29 @@
 package com.example.fuel_calculator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class PriceFuel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_price_fuel)
+
+        val priceFuel = findViewById<TextInputEditText>(R.id.edit_pricefuel)
+        val btnProximo = findViewById<Button>(R.id.btn_proximo)
+
+        btnProximo.setOnClickListener {
+
+            val intentSegunda = Intent (this, ConsumoCarro::class.java)
+            startActivity(intentSegunda)
+
+            println("tô aqui otário")
+
+        }
+
+
 
     }
 }
